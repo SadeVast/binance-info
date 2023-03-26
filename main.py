4e53ac4a38ee8475d93298c4f555a6a67a2b8ffd
VADESADE <vetalik.selikhov@mail.ru>
2023-03-26 20:56:20 +0300

    add docker-compose.yaml add requirements.txt update files

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def insert_binance_to_db():
         info_db.insert_into(info_db.create_connection(info_db.DATABASE),a)
 
 if __name__ == '__main__':
+    info_db.create_connection(info_db.DATABASE)
+    info_db.create_table(info_db.create_connection(info_db.DATABASE))
     insert_binance_to_db()
 
     for a in info_db.get_all_news(info_db.create_connection(info_db.DATABASE)):
